5a7ae9f86d8b1511f4f49230da4464dbdef91328
Felix Benning <felix.benning@gmail.com>
2023-06-01 17:41:01 +0200

    initial commit

--- src/ProductArrays.jl
+++ src/ProductArrays.jl
@@ -1,5 +1,74 @@
 module ProductArrays
 
-# Write your package code here.
+export productArray
+
+function _ensure_all_linear_indexed(vecs::T) where {T<:Tuple}
+    linear_indexed = ntuple(
+        n -> hasmethod(Base.getindex, (fieldtype(T, n), Int)),
+        Base._counttuple(T)
+    )
+    all(linear_indexed) || throw(ArgumentError(
+        "$(vecs[findfirst(x->!x, linear_indexed)]) cannot be linearly accessed. All inputs need to implement `Base.getindex(::T, ::Int)`"
+    ))
+end
+
+struct ProductArray{T<:Tuple,Eltype,N} <: AbstractArray{Eltype,N}
+    prodIt::Iterators.ProductIterator{T}
+    ProductArray(t::T) where {T} = begin
+        _ensure_all_linear_indexed(t)
+        prodIt = Iterators.ProductIterator(t)
+        new{T,eltype(Iterators.ProductIterator{T}),ndims(prodIt)}(prodIt)
+    end
+end
+
+# wrap ProductIterator
+function Base.IteratorSize(::Type{ProductArray{T,Eltype,N}}) where {T,Eltype,N}
+    Base.IteratorSize(Iterators.ProductIterator{T})
+end
+Base.size(p::ProductArray) = size(p.prodIt)
+Base.axes(p::ProductArray) = axes(p.prodIt)
+Base.ndims(::ProductArray{T,Eltype,N}) where {T,Eltype,N} = N
+Base.length(p::ProductArray) = length(p.prodIt)
+function Base.IteratorEltype(::Type{<:ProductArray{T}}) where {T}
+    Base.IteratorEltype(Iterators.ProductIterator{T})
+end
+Base.eltype(::Type{ProductArray{T,Eltype,N}}) where {T,Eltype,N} = Eltype
+Base.iterate(p::ProductArray) = iterate(p.prodIt)
+Base.iterate(p::ProductArray, state) = iterate(p.prodIt, state)
+
+Base.last(p::ProductArray) = last(p.prodIt)
+
+# implement private _getindex for ProductIterator
+
+function _getindex(prod::Iterators.ProductIterator, indices::Int...)
+    return _prod_getindex(prod.iterators, indices...)
+end
+_prod_getindex(::Tuple{}) = ()
+function _prod_getindex(p_vecs::Tuple, indices::Int...)
+    v = first(p_vecs)
+    n = ndims(v)
+    return (
+        v[indices[1:n]...],
+        _prod_getindex(Base.tail(p_vecs), indices[n+1:end]...)...
+    )
+end
+
+# apply this to ProductArray
+Base.getindex(p::ProductArray{T,Eltype,N}, indices::Vararg{Int,N}) where {T,Eltype,N} = _getindex(p.prodIt, indices...)
+
+"""
+    productArray(vectors...)
+
+The output is a lazy form of
+```julia
+collect(Iterators.product(vectors...))
+```
+i.e. it is an AbstractArray in contrast to `Iterators.product(vectors...)`. So
+is accessible with `getindex` and gets default Array implementations for free.
+In particular it can be passed to `Base.PermutedDimsArray`` for lazy permutation
+and `vec()` to obtain a lazy `Base.ReshapedArray`.
+"""
+productArray(vectors...) = ProductArray(vectors)
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,38 @@ using ProductArrays
 using Test
 
 @testset "ProductArrays.jl" begin
-    # Write your tests here.
+    @testset "ProductArray identical to collected Base.product" begin
+		@test productArray(1:3, 4:10) == collect(Base.product(1:3, 4:10))
+		v1 = rand(3, 2)
+		v2 = [:a, :b]
+		@test productArray(v1, v2) == collect(Base.product(v1, v2))
+
+		v = (v1, v2)
+		p = productArray(v...)
+
+		@testset "Test Type Functions" begin
+			@test Base.IteratorSize(typeof(p)) == Base.IteratorSize(typeof(p.prodIt))
+			@test Base.IteratorEltype(typeof(p)) == Base.IteratorEltype(typeof(p.prodIt))
+			@test Base.eltype(typeof(p)) == Base.eltype(typeof(p.prodIt))
+		end
+
+		c = collect(Base.product(v...))
+		@testset "Test Entries and Access" begin
+			@test p == c # same shape and entries
+			cart_idx = map(first, axes(p))
+			@test p[cart_idx...] == c[cart_idx...] # cartesian access
+			@test p[1] == c[1] # linear access
+			@test p[:] == c[:] # colon access
+			@test p[1:lastindex(p)] == vec(c) # range access
+			@test reverse(p) == reverse(c)
+			@test last(p) == p[map(last, axes(p))...]
+		end
+
+		@testset "Sanity Checks" begin
+			@test axes(p) == axes(c)
+			@test size(p) == map(length, axes(p))
+			@test length(p) == reduce(*, size(p))
+			@test axes(p, 1) == axes(p)[1]
+		end
+	end
 end
